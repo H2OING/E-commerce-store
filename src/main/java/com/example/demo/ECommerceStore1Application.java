9d8b92792975cdd72ccb4f13b79f760c434017c2
s21karkmark <s21karkmark@venta.lv>
2022-08-29 14:07:48 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/ECommerceStore1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceStore1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceStore1Application.class, args);
+	}
+
+}

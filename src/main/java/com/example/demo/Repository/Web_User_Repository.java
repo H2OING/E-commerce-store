a2a6ca8d808f2b27ba8f71c7e7f9f3ddcb572fc6
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2022-08-30 16:51:02 +0300

    added repository layers and bill service layer

---
+++ src/main/java/com/example/demo/Repository/Web_User_Repository.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.Web_User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Web_User_Repository extends JpaRepository<Web_User, Long> {
+}
